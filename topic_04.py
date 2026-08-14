c4fe8569afc89bd82f0f375f74914289e928c70f
saad <mail.saad0011@gmail.com>
2026-08-14 23:54:21 +0500

    ahow duplicate path operation behavior for same GET route

---
+++ topic_04.py
@@ -0,0 +1,20 @@
+"""
+    Redefining a Path Operation Function
+    - You can redefine a path operation function in FastAPI by using the same path and HTTP method.
+    - The return value of first path operation will be used in Swagger UI and OpenAPI documentation.
+    - While the function name of second function will be used in Swagger UI.
+
+    Test the application by changing the order of the two path operations.
+"""
+
+
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get('/users')  
+async def read_users_again():
+    return ['Tom', 'Jerry']
+
+@app.get('/users')
+async def read_users():
+    return ["Rick", "Morty"]
