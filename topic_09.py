a8437276e5e4cdf0557050f76e15bcff5c007ff9
saad <mail.saad0011@gmail.com>
2026-08-15 19:24:44 +0500

    fix: added key assignment to dictionary

--- topic_09.py
+++ topic_09.py
@@ -26,7 +26,7 @@ app = FastAPI()
 async def read_item(item_id: str, q: str | None = None, short: bool = False):  # short: Query Parameter with default value and type conversion 
     item = {'item_id': item_id}
     if q:
-        item.update({'q': q})
+        item["q"] = q       # direct key assignment to the dictionary
     if not short:
         item.update({'description': 'This is an amazing item that has a long description'})
     return item
