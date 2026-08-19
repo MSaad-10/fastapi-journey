891f252e1fad78a366d61b1ef16912a65ca08c40
saad <mail.saad0011@gmail.com>
2026-08-19 18:13:40 +0500

    docs: uncommented the code for request body + path parameters

--- topic_13.py
+++ topic_13.py
@@ -6,20 +6,20 @@
 """
 
 
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# class Item(BaseModel):
-#     name: str
-#     description: str | None = None
-#     price: float
-#     tax: float | None = None
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.put("/items/{item_id}")                        # works same as @app.post()
-# async def update_item(item_id: int, item: Item):
-#     return {"item_id": item_id, **item.model_dump()}
+@app.put("/items/{item_id}")                        # works same as @app.post()
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
 
 
 """
