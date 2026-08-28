5602f9019c5d5ae8ef444bc4a4b107bb6c7f2a39
saad <mail.saad0011@gmail.com>
2026-08-28 17:40:02 +0500

    feat: extend and reuse default FastAPI exception handlers

---
+++ 17. Handling Errors/topic_80.py
@@ -0,0 +1,33 @@
+"""
+    REUSE FastAPI's EXCEPTION HANDLERS
+    - If you want to use the exception along with the same default exception handlers from FastAPI.
+    - You can import and reuse the default exception handlers from fastapi.exception_handlers.
+    - You can use the exception and then just reuse the default exception handlers.
+"""
+
+
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+app = FastAPI()
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f'OMG! An HTTP error!: {repr(exc)}')
+    return await http_exception_handler(request, exc)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f'OMG! The client sent invalid data!: {exc}')
+    return await request_validation_exception_handler(request, exc)
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {'item_id': item_id}
